504c177ca941031aabe42a0a22d44084c44cf014
javohir-abdunabiyev <jahaajahaaa2@gmail.com>
2025-04-18 18:13:04 +0500

    added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button";
 import "./globals.css";
-
+import { LuHash } from "react-icons/lu";
+import { GrHomeRounded } from "react-icons/gr";
+import { IoSearchOutline } from "react-icons/io5";
+import { FaPlus } from "react-icons/fa6";
+import { IoMdHeartEmpty } from "react-icons/io";
+import { BsPerson } from "react-icons/bs";
 
 export default function RootLayout({
   children,
@@ -8,6 +14,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <aside>
+        <div>
+          <LuHash color="white" size={34} className="cursor-pointer" />
+        </div>
+        <div className="flex flex-col gap-[20px] mt-4">
+          <Button className="w-[65px] h-[45px] cursor-pointer">
+            <GrHomeRounded color="rgb(77, 77, 77)" className="!w-[24px] !h-[24px]" />
+          </Button>
+          <Button className="w-[65px] h-[45px] cursor-pointer">
+            <IoSearchOutline color="rgb(77, 77, 77)" className="!w-[30px] !h-[30px]" />
+          </Button>
+          <Button className="w-[65px] h-[45px] cursor-pointer">
+            <FaPlus color="rgb(77, 77, 77)" className="!w-[30px] !h-[30px]" />
+          </Button >
+          <Button className="w-[65px] h-[45px] cursor-pointer">
+            <IoMdHeartEmpty color="rgb(77, 77, 77)" className="!w-[30px] !h-[30px]" />
+          </Button>
+          <Button className="w-[65px] h-[45px] cursor-pointer">
+            <BsPerson color="rgb(77, 77, 77)" className="!w-[30px] !h-[30px]" />
+          </Button>
+        </div>
+      </aside>
       <body>
         {children}
       </body>
